ede5403717ea6106e5f045da0cb205e18f67061a
MarynaPR <pryadkamaryna@gmail.com>
2020-11-09 14:54:16 -0600

    updated code

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { animals } = require('./data/animals');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -72,7 +73,10 @@ app.get('/api/animals', (req, res) => {
 // app.get('/api/animals', (req, res) => {
 //     res.send('Hello!');
 // });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
+// app.listen(3001, () => {
+//     console.log(`API server now on port 3001!`);
+// });
 
