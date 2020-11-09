68af8fc2e81a2e67c9acf4d08a009c83cd6e0b64
MarynaPR <pryadkamaryna@gmail.com>
2020-11-09 16:11:29 -0600

    web server created

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
+    //let personalityTraitsArray = [];
     // Note that we save the animalsArray as filteredResults here:
     let filteredResults = animalsArray;
     if (query.personalityTraits) {
@@ -73,10 +73,26 @@ app.get('/api/animals', (req, res) => {
 // app.get('/api/animals', (req, res) => {
 //     res.send('Hello!');
 // });
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
 // app.listen(3001, () => {
 //     console.log(`API server now on port 3001!`);
 // });
+// app.get('/api/animals/:id', (req, res) => {
+//     const result = findById(req.params.id, animals);
+//     res.json(result);
+// });
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
 
